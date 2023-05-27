9727637a5288857d6e3191fcd16603a50e34e281
sw-tracker <sw-tracker@users.noreply.github.com>
2023-05-27 20:52:53 +0000

    deploy: ffbc4e5dc7a2815e630a8b79dc09cf033d2d8a49

--- src/app/utils/utils.ts
+++
@@ -1,5 +0,0 @@
-import { isNull, isUndefined } from 'lodash-es';
-
-export const isNullOrUndefined = (value: unknown): value is null | undefined => isNull(value) || isUndefined(value);
-
-export const isNotNullOrUndefined = <T>(value: T | undefined | null): value is T => !isNullOrUndefined(value);
